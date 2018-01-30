cb7e5266679f1e69403a02b2a8591386cf13e80f
Unknown <jr0850050@otc.edu>
2018-01-29 23:03:20 -0600

    initializing php
    figuring out php

--- index.php
+++ index.php
@@ -8,26 +8,20 @@
     <meta name="description" content="Product Discount">
     <meta name="author" content="Joshua Ratliff">
 
-    <!-- bootstrap css -->
+    <!--css -->
     <link href='http://fonts.googleapis.com/css?family=Open+Sans+Condensed:300' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="styles.css">
 
 </head>
-<script>
-  function adjust_textarea(h) {
-      h.style.height = "20px";
-      h.style.height = (h.scrollHeight)+"px";
-  }
-</script>
+
 <body>
     <!-- form block for grabbing description, list price, and discount -->
     <div class="form-style-8" id="form">
       <h2>Please enter your information</h2>
-        <form>
-          <input type="text" name="field1" placeholder="Product Description" />
-          <input type="number" name="field2" placeholder="List Price" />
-          <input type="number" name="field3" placeholder="Discount" />
-          <textarea placeholder="Message" onkeyup="adjust_textarea(this)"></textarea>
+        <form action='product.php'>
+          <input type="text" name="field1" placeholder="Product Description" name="description"/>
+          <input type="number" name="field2" placeholder="List Price" name="price"/>
+          <input type="number" name="field3" placeholder="Discount" name="discount"/>
           <input type="button" value="Submit" />
         </form>
     </div>
--- product.php
+++ product.php
@@ -1,10 +1,65 @@
 <?php
 
 $description = $_GET['description'];
-$description = $_GET['price'];
-$description = $_GET['discount'];
+$price = $_GET['price'];
+$discount = $_GET['discount'];
 
 $discountAmount = $price * ($discount / 100);
-$total = $price - $discountAmount;
+$total = $price - $discountAmount; 
 
 ?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>Product Discount Calculator</title>
+  </head>
+  <body>
+  <div class="container" style="height: 100vh;">
+    <div class="row align-items-center" style="height: 100%;">
+        <div class="col-sm"></div>
+        <div class="col-sm-6">
+            <div class="card">
+                <div class="card-header">
+                    Discount Calculator
+                </div>
+                <div class="card-body">
+                    <dl class="row">
+                        <dt class="col-6">Description</dt>
+                        <dd class="col-6"><?= $description ?></dd>
+
+                        <dt class="col-6">List Price</dt>
+                        <dd class="col-6">$<?= $price ?></dd>
+
+                        <dt class="col-6">Discount</dt>
+                        <dd class="col-6"><?= $discount ?>%</dd>
+
+                        <dt class="col-6">Discount Amount</dt>
+                        <dd class="col-6">$<?= $discountAmount ?></dd>
+
+                        <dt class="col-6">Discount Price</dt>
+                        <dd class="col-6">$<?= $total ?></dd>
+                    </dl>
+
+                    <a href="/" class="btn btn-primary">Go Back</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm"></div>
+    </div>
+  </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
