5235c1196b0a02c8c5168beb095c09cd999c9f8d
Unknown <jr0850050@otc.edu>
2018-01-31 09:44:42 -0600

    fixing submit

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
+<?php
+
+    $products = [
+        "Screwdriver",
+        "Clapton Coil",
+        "WD Red 2TB HDD 7200rpm (x4)",
+        "16GB 4x4 DDR4 RAM"
+    ];
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -19,10 +30,15 @@
     <div class="form-style-8">
       <h2>Please enter your information</h2>
         <form action='product.php' method="get">
-          <input type="text" name="field1" placeholder="Product Description" name="description"/>
+          <!-- <input type="text" name="field1" placeholder="Product Description" name="description"> -->
+          <select name="description">
+                <?php foreach ($products as $product): ?>
+                    <option value="<?= $product ?>"><?= $product ?></option>
+                <?php endforeach; ?>
+            </select>
           <input type="number" name="field2" placeholder="List Price" name="price"/>
           <input type="number" name="field3" placeholder="Discount" name="discount"/>
-          <input type="button" value="Submit" />
+          <input type="submit" value="Submit" />
         </form>
     </div>
 </body>
