00099b2d63234299757a3ecef5c3c82b6fe56568
Unknown <jr0850050@otc.edu>
2018-02-17 22:16:14 -0600

    Fixing php... again!
    I feel like the vast majority of my commits are going to be like this. regardless, the buttons work again. i also formatted the pages to look pretty.

--- index.php
+++ index.php
@@ -59,28 +59,36 @@ require_once('./db.php');
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="styles.css">
 
-</head>
+    <body>
+    <div class="form-style-8">
+      <h2 class="card-header">Please select your product</h2>
+        <form action="<?= array_key_exists('debug', $_GET) ? 'debug.php' : 'product.php' ?>">
+            <div class="form-group">
+                <label for="product_id">Product</label>
+                <select name="product_id" id="product_id" class="form-control">
+                    <?php foreach($products as $product): ?>
+                        <option value="<?= $product['id'] ?>"><?= $product['name'] ?> - $<?= $product['price'] ?></option>
+                    <?php endforeach; ?>
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="coupon_id">Discount</label>
+                <select name="coupon_id" id="coupon_id" class="form-control">
+                    <?php foreach($coupons as $coupon): ?>
+                        <option value="<?= $coupon['id'] ?>"><?= $coupon['code'] ?> - <?= $coupon['discount_percent'] ?>% Off</option>
+                    <?php endforeach; ?>
+                </select>
+            </div>
 
-<body>
-  <!-- form block for grabbing description, list price, and discount -->
-  <div class="form-style-8">
-    <h2>Please enter your information</h2>
-          <!-- product foreach block -->
-            <label for="product_id" class="outset">Product</label>
-            <select name="product_id" id="product_id" class="form-control">
-                <?php foreach($products as $product): ?>
-                    <option value="<?= $product['id'] ?>"><?= $product['name'] ?> - $<?= $product['price'] ?></option>
-                <?php endforeach; ?>
-            </select>
-          <!-- discount foreach block -->
-            <label class="outset" for="coupon_id">Discount</label>
-            <select name="coupon_id" id="coupon_id" class="form-control">
-                <?php foreach($coupons as $coupon): ?>
-                    <option value="<?= $coupon['id'] ?>"><?= $coupon['code'] ?> - <?= $coupon['discount_percent'] ?>% Off</option>
-                <?php endforeach; ?>
-              </select>
-          <input type="submit" value="Submit" />
-      </form>
-  </div>
-</body>
-</html>
+            <button class="btn btn-primary" type="submit" value="Submit">Submit</button>
+        </form>
+      </div>
+    </div>
+
+      <!-- Optional JavaScript -->
+      <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+      <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    </body>
+    </html>
--- product.php
+++ product.php
@@ -47,55 +47,69 @@ $total = $price - $discountAmount;
 
     <!--css -->
     <link href='http://fonts.googleapis.com/css?family=Open+Sans+Condensed:300' rel='stylesheet' type='text/css'>
-    <link rel="stylesheet" href="styles.css">
-
-    <title>Product Discount Calculator</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles.css">
 
     <title>Product Discount Calculator</title>
   </head>
   <body>
-    <div class="container" style="height: 100vh;">
-      <div class="row align-items-center" style="height: 100%;">
-          <div class="col-sm"></div>
-          <div class="col-sm-6">
-              <div class="card">
-                  <div class="card-header">
-                      Discount Calculator
-                  </div>
-                  <div class="card-body">
-                      <dl class="row">
-                          <dt class="col-6">Description</dt>
-                          <dd class="col-6"><?= $description ?></dd>
+    <div class="form-style-8">
+      <h2 class="card-header" stlye="text-align:center">Thank you for shopping at Joshmart!</h2>
+        <dl class="row">
+            <dt class="col-6">Description</dt>
+            <dd class="col-6"><?= $description ?></dd>
 
-                          <dt class="col-6">List Price</dt>
-                          <dd class="col-6">$<?= $price ?></dd>
+            <dt class="col-6">List Price</dt>
+            <dd class="col-6">$<?= $price ?></dd>
 
-                          <dt class="col-6">Coupon</dt>
-                          <dd class="col-6"><?= $coupon['code'] ?> - <?= $coupon['description'] ?></dd>
+            <dt class="col-6">Coupon</dt>
+            <dd class="col-6"><?= $coupon['code'] ?> - <?= $coupon['description'] ?></dd>
 
-                          <dt class="col-6">Discount</dt>
-                          <dd class="col-6"><?= $discount ?>%</dd>
+            <dt class="col-6">Discount</dt>
+            <dd class="col-6"><?= $discount ?>%</dd>
 
-                          <dt class="col-6">Discount Amount</dt>
-                          <dd class="col-6">$<?= $discountAmount ?></dd>
+            <dt class="col-6">Discount Amount</dt>
+            <dd class="col-6">$<?= $discountAmount ?></dd>
 
-                          <dt class="col-6">Discount Price</dt>
-                          <dd class="col-6">$<?= $total ?></dd>
-                      </dl>
-
-                      <a href="/" class="btn btn-primary">Go Back</a>
-                  </div>
-              </div>
-          </div>
-          <div class="col-sm"></div>
-      </div>
+            <dt class="col-6">Discount Price</dt>
+            <dd class="col-6">$<?= $total ?></dd>
+        </dl>
+      <a href="index.php" class="btn btn-primary">Go Back</a>
     </div>
+  <!-- <div class="form-style-8">
+    <div class="row align-items-center" style="height: 100%;">
+            <div class="card">
+                <div class="card-body">
+                    <dl class="row">
+                        <dt class="col-6">Description</dt>
+                        <dd class="col-6"><?= $description ?></dd>
 
-      <!-- Optional JavaScript -->
-      <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-      <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    </body>
-  </html>
+                        <dt class="col-6">List Price</dt>
+                        <dd class="col-6">$<?= $price ?></dd>
+
+                        <dt class="col-6">Coupon</dt>
+                        <dd class="col-6"><?= $coupon['code'] ?> - <?= $coupon['description'] ?></dd>
+
+                        <dt class="col-6">Discount</dt>
+                        <dd class="col-6"><?= $discount ?>%</dd>
+
+                        <dt class="col-6">Discount Amount</dt>
+                        <dd class="col-6">$<?= $discountAmount ?></dd>
+
+                        <dt class="col-6">Discount Price</dt>
+                        <dd class="col-6">$<?= $total ?></dd>
+                    </dl>
+
+                    <a href="index.php" class="btn btn-primary">Go Back</a>
+                </div>
+            </div>
+        </div> -->
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
